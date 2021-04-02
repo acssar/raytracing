afa3ddf550bd29fd136e472e567fe3a29b4547ef
Roman <romaderunec@gmail.com>
2021-04-02 20:26:47 +0700

    gui update

--- app/Main.hs
+++ app/Main.hs
@@ -11,30 +11,48 @@ destroyEv = do mainQuit
 
 main :: IO ()
 main = do
-  rayfunc
+  --rayfunc
   initGUI
-  window <- windowNew
-  onDelete window deleteEv
-  onDestroy window destroyEv
-  windowSetTitle window "Haskell raytracer"
-  scrollWindow <- scrolledWindowNew Nothing Nothing
-  containerSetBorderWidth scrollWindow 10
-  image <- imageNewFromFile "result.ppm"
-  scrolledWindowAddWithViewport scrollWindow image
-  widgetSetSizeRequest scrollWindow 800 600
 
   controlPanel <- vBoxNew False 2
   containerSetBorderWidth controlPanel 10
 
-  entry <- entryNew
-  entrySetWidthChars entry 20
+  messageLabel <- labelNew (Just "\nDon't press this button.\n")
 
-  hBox <- hBoxNew False 2
-  boxPackStart hBox scrollWindow PackNatural 0
-  boxPackStart hBox controlPanel PackNatural 0
-  containerAdd window hBox
-  --boxPackStart controlPanel entry PackNatural 0
+  coolButton <- buttonNewWithLabel "Render Button"
+  --load button
+  --  entry <- entryNew
+  --  entrySetWidthChars entry 20
 
-  widgetShowAll window
+  window2 <- windowNew
+  onDelete window2 deleteEv
+  onDestroy window2 destroyEv
+  windowSetTitle window2 "Raytrace window"
+--  scrollWindow2 <- scrolledWindowNew Nothing Nothing
+  hBox2 <- hBoxNew False 2
+--  boxPackStart hBox2 scrollWindow2 PackNatural 0
+  boxPackStart hBox2 controlPanel PackNatural 0
+  boxPackStart controlPanel coolButton PackNatural 0
+  containerAdd window2 hBox2
+  widgetShowAll window2
+
+  onClicked coolButton $ do
+    labelSetText messageLabel "\nOh, no, you pressed the button !\n"
+    rayfunc
+    window <- windowNew
+    --onDelete window deleteEv
+    --onDestroy window destroyEv
+    windowSetTitle window "Haskell raytracer"
+    scrollWindow <- scrolledWindowNew Nothing Nothing
+    containerSetBorderWidth scrollWindow 10
+    image <- imageNewFromFile "result.ppm"
+    scrolledWindowAddWithViewport scrollWindow image
+    widgetSetSizeRequest scrollWindow 800 600
+    hBox <- hBoxNew False 2
+    boxPackStart hBox scrollWindow PackNatural 0
+    containerAdd window hBox
+    widgetShowAll window
+
+  boxPackStart controlPanel messageLabel PackNatural 0
 
   mainGUI
