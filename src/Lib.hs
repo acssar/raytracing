40543bba7ad0958c68482753712a024ee49e6f72
Roman <romaderunec@gmail.com>
2021-04-06 22:54:35 +0700

    add intRayWith Triangle

--- src/Lib.hs
+++ src/Lib.hs
@@ -15,7 +15,7 @@ data Ray = Ray Point3D Vector
 
 data Object = Sphere Double Point3D
  | Plane (Double,Double,Double,Double)
-
+ | Triangle (Point3D, Point3D, Point3D)
 
 type Resolution = (Int,Int)
 type Dimension = (Int,Int)
@@ -86,6 +86,9 @@ minF f (x,y,z) = (min x f, min y f, min z f)
 (*.) :: Vector -> Vector -> Double  --scalar
 (x1,y1,z1) *. (x2,y2,z2) = x1*x2 + y1*y2 + z1*z2
 
+crossProduct :: Vector -> Vector -> Vector
+crossProduct (x1,y1,z1) (x2,y2,z2) = (y1*z2 - z1*y2, z1*x2 - x1*z2, x1*y2 - y1*x2)
+
 len :: Vector -> Double   --length
 len v = sqrt (v *. v)
 
@@ -93,6 +96,8 @@ norm :: Vector -> Vector  --normalize
 norm v
  | len v < 10**(-9) = (0.0,0.0,0.0)
  | otherwise = v *> (1/(len v))
+
+
 
 mkNormVect :: Point3D -> Point3D -> Vector --MakeNormalizedVector
 mkNormVect v w = norm (w <-> v)
@@ -133,21 +138,69 @@ mkRay p1 p2 = Ray p1 (mkNormVect p1 p2)
 intRayWith :: Ray -> Object -> [Double]
 intRayWith (Ray start dir) (Sphere rad cen) = solveq (dir *. dir, 2*(dir *. d), (d *. d) - rad^2)
  where
- d = start <-> cen
+   d = start <-> cen
 intRayWith (Ray start dir) (Plane (a,b,c,d)) = if (abs(part) < 10**(-9)) then [] else [- (d + ((a,b,c) *. start) ) / part]
  where
- part = (a,b,c) *. dir
+  part = (a,b,c) *. dir
+intRayWith (Ray (x0,y0,z0) (p1,p2,p3)) (Triangle ((x1,y1,z1),(x2,y2,z2),(x3,y3,z3))) =
+  if (pointInTriangle (x,y,z) (x1,y1,z1) (x2,y2,z2) (x3,y3,z3) )then intRayWith (Ray (x0,y0,z0) (p1,p2,p3)) pl
+  else []
+  where
+  pl@(Plane (aa,bb,cc,dd)) = planefromtriangle (Triangle ((x1,y1,z1),(x2,y2,z2),(x3,y3,z3)))
+  y = (p3*cc*y0 + aa*p1*y0 - p2*aa*x0 - p2*cc*z0)/(aa*p1 + bb + cc*p3)
+  x = (p1*y - p1*y0 + p2*x0)/p2
+  z = (p3*y -p3*y0 + p2*z0)/p2
+-- normal for objects (TODO: square, triangle etc.)
 
+planefromtriangle :: Object -> Object
+planefromtriangle (Triangle ((x1,y1,z1),(x2,y2,z2),(x3,y3,z3))) = Plane (aa,bb,cc,dd)
+ where
+  aa = (y2-y1)*(z3-z1) - (y3-y1)*(z2-z1)
+  bb = (x3-x1)*(z2-z1) - (x2-x1)*(z3-z1)
+  cc = (x2-x1)*(y3-y1) - (x3-x1)*(y2-y1)
+  dd = (-1.0)*(aa + bb + cc)
 
- -- normal for objects  (TODO: square, triangle etc.)
-
+ -- normal for objects  (TODO: square, triangle etc.
 
 normal :: Point3D -> Object -> Vector
 normal p (Sphere rad cen) = norm ((p <-> cen) *> (1/rad))
 normal _ (Plane (a,b,c,d)) = norm (a,b,c)
+normal _ (Triangle ((x1,y1,z1),(x2,y2,z2),(x3,y3,z3))) = norm (p,q,s) where
+  p = (y2-y1)*(z3-z2) - (y3-y1)*(z2-z1)
+  q = (x3-x2)*(z2-z1) - (x2-x1)*(z3-z2)
+  s = (x2-x1)*(y3-y2) - (x3-x2)*(y2-y1)
 
+sameSide :: Point3D -> Point3D -> Point3D -> Point3D -> Bool
+sameSide p1@(xp1,yp1,zp1) p2@(xp2,yp2,zp2) a@(xa,ya,za) b@(xb,yb,zb) = if ((cp1) *. (cp2) >= 0) then True else False
+      where
+      vec1 = (xb-xa,yb-ya,zb-za)
+      vec2 = (xp1-xa,yp1-ya,zp1-za)
+      vec3 = (xp2-xa,yp2-ya,zp2-za)
+      cp1 = crossProduct vec1 vec2
+      cp2 = crossProduct vec1 vec3
 
+pointInTriangle :: Point3D -> Point3D -> Point3D -> Point3D -> Bool
+pointInTriangle p a b c = if ((sameSide p a b c) && (sameSide p b a c) && (sameSide p c a b)) then True else False
+-- baricentric_test :: Ray -> Object
 -- reflection of vector
+
+
+-------------baracentric check ( can use instead of sameSide and (pointInTriangle))--- t = 1 -u -v
+baracentric :: Ray -> Object -> Bool
+baracentric (Ray pr vr) (Triangle (a, b, c)) = if (t == 1 - u - v) then True else False
+  where
+    e1 = b <-> a
+    e2 = c <-> a
+    t_ = pr <-> a
+    d = vr
+    p = crossProduct d e2
+    q = crossProduct t_ e1
+    t = (1 / (p *. e1)) * (q *. e2)
+    u = (1 / (p *. e1)) * (p *. t_)
+    v = (1 / (p *. e1)) * (q *. d)
+
+
+
 
 
 reflectDir :: Vector -> Vector -> Vector
@@ -346,59 +399,87 @@ getLights h1 inp = do
 scanfTexturedObjects :: Handle -> FilePath -> Int -> [TexturedObject] -> IO ([TexturedObject])
 
 scanfTexturedObjects h file 0 xs = do
-  return (xs)
+	return (xs)
 
 scanfTexturedObjects h file n xs = do
-  str <- hGetLine h
-  let obj = read str
-  str <- hGetLine h
-  let tex = read str
-  str <- hGetLine h
-  let c1 = read str
-  str <- hGetLine h
-  let c2 = read str
-  str <- hGetLine h
-  let c3 = read str
-  str <- hGetLine h
-  let c4 = read str
-  str <- hGetLine h
-  let t1 = read str
-  str <- hGetLine h
-  let t2 = read str
-  str <- hGetLine h
-  let t3 = read str
-  str <- hGetLine h
-  let t4 = read str
-  str <- hGetLine h
-  let tint = read str
-  str <- hGetLine h
-  let t5 = read str
-  str <- hGetLine h
-  let t6 = read str
-  let ss = (Sphere c1 (c2,c3,c4),Texture ( Solid (t1,t2,t3) ) t4 tint t5 t6)
-  let ps = (Plane (c1,c2,c3,c4),Texture ( Solid (t1,t2,t3) ) t4 tint t5 t6)
-  if(obj == 1) then if (tex == 1) then scanfTexturedObjects h file (n-1) (ps:[] ++ xs) else error "perlin is not ready" else if (tex == 1) then scanfTexturedObjects h file (n-1) (ss:[] ++ xs) else error "perlin is not ready"
+	str <- hGetLine h
+ 	let obj = read str
+ 	if (obj == 3) then getTriangle h file n xs else buffunc obj h file n xs
 
--- 1 - plane
--- 2 - sphere
--- s1 = Sphere 15.0 (0.0,0.0,-400.0)
--- s2 = Sphere 15.0 (30.35,-5.0,-300.35)
--- p1 = Plane (0.0,10.0,0.0,300.0)
+getTriangle :: Handle -> FilePath -> Int -> [TexturedObject] -> IO ([TexturedObject])
+getTriangle h file n xs = do
+	str <- hGetLine h
+ 	let tex = read str
+ 	str <- hGetLine h
+ 	let x1 = read str
+ 	str <- hGetLine h
+ 	let y1 = read str
+ 	str <- hGetLine h
+ 	let z1 = read str
+ 	str <- hGetLine h
+ 	let x2 = read str
+ 	str <- hGetLine h
+ 	let y2 = read str
+ 	str <- hGetLine h
+ 	let z2 = read str
+ 	str <- hGetLine h
+ 	let x3 = read str
+ 	str <- hGetLine h
+ 	let y3 = read str
+ 	str <- hGetLine h
+ 	let z3 = read str
+ 	str <- hGetLine h
+ 	let t1 = read str
+ 	str <- hGetLine h
+ 	let t2 = read str
+ 	str <- hGetLine h
+ 	let t3 = read str
+ 	str <- hGetLine h
+ 	let t4 = read str
+ 	str <- hGetLine h
+ 	let tint = read str
+ 	str <- hGetLine h
+ 	let t5 = read str
+ 	str <- hGetLine h
+ 	let t6 = read str
+ 	let trito = (Triangle ((x1,y1,z1),(x2,y2,z2),(x3,y3,z3)), Texture ( Solid (t1,t2,t3) ) t4 tint t5 t6)
+ 	if (tex == 1) then scanfTexturedObjects h file (n-1) (trito:[] ++ xs) else error "perlin is not ready"
 
--- t1 = Texture ( Solid (0.8,0.8,0.8) ) 1.0 60 0.0 0.0
--- t2 = Texture ( Solid (0.8,0.0,0.0) ) 0.8 60 0.0 0.0
--- t3 = Texture ( Solid (0.5,0.5,0.5) ) 0.8 60 0.0 0.0
-
--- to1 = (s1,t1)
--- to2 = (s2,t2)
--- to3 = (p1,t3)
+buffunc :: Integer -> Handle -> FilePath -> Int -> [TexturedObject] -> IO ([TexturedObject])
+buffunc obj h file n xs = do
+	str <- hGetLine h
+ 	let tex = read str
+	str <- hGetLine h
+ 	let c1 = read str
+ 	str <- hGetLine h
+ 	let c2 = read str
+ 	str <- hGetLine h
+ 	let c3 = read str
+ 	str <- hGetLine h
+ 	let c4 = read str
+ 	str <- hGetLine h
+ 	let t1 = read str
+ 	str <- hGetLine h
+ 	let t2 = read str
+ 	str <- hGetLine h
+ 	let t3 = read str
+ 	str <- hGetLine h
+ 	let t4 = read str
+ 	str <- hGetLine h
+ 	let tint = read str
+ 	str <- hGetLine h
+ 	let t5 = read str
+ 	str <- hGetLine h
+ 	let t6 = read str
+ 	let ss = (Sphere c1 (c2,c3,c4),Texture ( Solid (t1,t2,t3) ) t4 tint t5 t6)
+ 	let ps = (Plane (c1,c2,c3,c4),Texture ( Solid (t1,t2,t3) ) t4 tint t5 t6)
+ 	if(obj == 1) then if (tex == 1) then scanfTexturedObjects h file (n-1) (ps:[] ++ xs) else error "perlin is not ready" else if (tex == 1) then scanfTexturedObjects h file (n-1) (ss:[] ++ xs) else error "perlin is not ready"
 
 getTexturedObjects :: Handle -> FilePath -> IO ([TexturedObject])
 getTexturedObjects h1 inp = do
-        str <- hGetLine h1
-        let n = read str :: Int
-        scanfTexturedObjects h1 inp n []
-
+          str <- hGetLine h1
+          let n = read str :: Int
+          scanfTexturedObjects h1 inp n []
 -----------------------------------------------------------reading background----------------------------------------------------------------
 
 getBack ::Handle -> FilePath -> IO (Color)
